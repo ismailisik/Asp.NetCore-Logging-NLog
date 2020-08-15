9dd86ef13de8fe8984f0b234ed789063a0ace05a
ismailisik <i.isikl@outlook.com.tr>
2020-08-15 21:52:24 +0300

    Initial Project

---
+++ Asp.NetCore.Logging.Nlog/Asp.NetCore.Logging.Nlog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Asp.NetCore.Logging.Nlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
